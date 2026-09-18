2a12e3b011a9bb52f5fbad534e099be0fef753d8
MAU250717 <MAU250717@spengergasse.at>
2026-09-18 15:26:26 +0200

    refactor: new add function + refactored

--- main.js
+++ main.js
@@ -25,3 +25,33 @@ const mixed = [1, "Ana", true];
 //const name = "Fabian";
 //const greetingString = greetings(name);
 //console.log(greetingString);
+
+// Polymorph
+function add(number1, number2)
+{
+    if (typeof number1 === "number" && number1 > 0) {
+        if (typeof number2 === "number" && number2 > 0) {
+            // Happy Path
+            return number1 + number2;
+        } else {
+            // throw new Error(`Das ist keine positive Number ${number2}`);
+            return `Das ist keine positive Number ${number2}`;
+            // return "Das ist keine Number 1" + number2;
+        }
+    } else {
+        return `Das ist keine positive Number ${number1}`;
+    }
+}
+
+// Refactored
+const err = `Das ist keine positive Number`
+function add(number1,number2) {
+    if(typeof number1 !== "number" && number1 <= 0){
+        throw new Error(err + number1);
+    }
+    if(typeof number2 !== "number" && number2 <= 0) {
+        throw new Error(err + number2);
+    }
+
+    return number1 + number2;
+}
